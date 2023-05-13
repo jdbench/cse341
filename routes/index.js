ee0efec9ddd95d58225a26698e2c26e72f9ce10a
BuildTools <justin.d.bench@gmail.com>
2023-05-13 14:55:11 -0600

    complete week 4

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
-const { nameFunction } = require('../controllers');
-const { getData } = require('../controllers/professional');
+const { nameFunction } = require("../controllers");
+const { getData } = require("../controllers/professional");
 const contacts = require("./contacts.js");
+const swagger = require("./swagger.js");
 
-const routes = require('express').Router();
+const routes = require("express").Router();
 
-routes.get('/', nameFunction);
-routes.get('/professional', getData)
+routes.get("/", nameFunction);
+routes.get("/professional", getData);
 routes.use("/contacts", contacts);
+routes.use("/api-docs", swagger);
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Z-Key");
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Methods", "Get, POST, PUT, DELETE, OPTIONS");
     next();
   })
   .use("/", routes);
